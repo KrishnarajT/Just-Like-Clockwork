51524081310b55875d5e2cd6065a2347ba874304
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2025-07-07 13:49:10 +0530

    added lots of svgs for starting screen

---
+++ src/components/ui/RandomSVG.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const svgs = import.meta.glob("../../assets/svgs/*.svg", { eager: true });
+
+export default function RandomSVG() {
+  const [src, setSrc] = useState(null);
+
+  useEffect(() => {
+    const keys = Object.keys(svgs);
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    setSrc(svgs[randomKey].default);
+  }, []);
+
+  return (
+    <div className="w-full flex justify-center">
+      {src && <img src={src} alt="Random SVG" className="w-[20vw]" />}
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,7 @@ import { StartButton } from "../components/ui/StartButton";
 import TimerContinuous from "../components/TimerContinuous";
 
 import Footer from "../components/Footer";
+import RandomSVG from "../components/ui/RandomSVG";
 const Home = () => {
 	const { theme } = React.useContext(ThemeContext);
 	useEffect(() => {
@@ -168,7 +169,7 @@ const Home = () => {
 				<button>
 					{started === false ? (
 						<StartButton
-							className="w-24 h-24 text-secondary transition-all duration-300 hover:text-primary hover:scale-90"
+							className="w-24 h-24 text-accent transition-all duration-300 hover:text-primary hover:scale-90"
 							onClick={() => {
 								if (laps.length === 0) {
 									const new_lap_id = addNewLap();
@@ -183,7 +184,7 @@ const Home = () => {
 						/>
 					) : (
 						<StopButton
-							className="w-24 h-24 text-error"
+							className="w-24 h-24 text-error transition-all duration-300 hover:scale-90"
 							onClick={() => {
 								setStarted(false);
 								setIsPlaying(false);
@@ -196,52 +197,68 @@ const Home = () => {
 			</div>
 
 			{/* table */}
-			<div className="overflow-x-auto m-6 outline outline-secondary rounded-2xl scroll-auto">
-				<table className="table table-lg text-xl">
-					{/* head */}
-					<thead className="">
-						<tr className="text-2xl">
-							<th className="w-4">ID</th>
-							<th className="w-48">Start Time</th>
-							<th className="w-48">End Time</th>
-							<th className="w-12">Elapsed Time</th>
-							<th className="w-1/3 break-words">Work Done</th>
-							<th className="w-12">Amount</th>
-						</tr>
-					</thead>
-					<tbody>
-						{/* body */}
-						{laps.map((lap, index) => {
-							return (
-								<tr key={index + 1} className="text-2xl">
-									<td className="w-fit text-2xl">{index + 1}</td>
-									<td className="text-2xl">{lap.getStartTime()}</td>
-									<td className="text-2xl">
-										{lap.getEndTime() === 0
-											? "Not finished yet"
-											: lap.getEndTime()}
-									</td>
-									<td className="text-2xl">
-										{lap.getCurrentHours()}h: {lap.getCurrentMinutes()}m:{" "}
-										{lap.getCurrentSeconds()}s
-									</td>
-									<td className="text-2xl break-words">
-										<textarea
-											className="textarea textarea-bordered w-full rounded-lg text-xl"
-											placeholder="Type here"
-											value={lap.getWorkDoneString()}
-											onChange={(e) => {
-												updateWorkDoneByID(lap.getId(), e.target.value);
-											}}
-										></textarea>
-									</td>
-									<td className="text-2xl">{lap.getAmount()}</td>
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
-			</div>
+			{laps.length > 0 ? (
+				<div className="overflow-x-auto m-6 outline outline-secondary rounded-2xl scroll-auto">
+					<table className="table table-lg text-xl">
+						{/* head */}
+						<thead className="">
+							<tr className="text-2xl">
+								<th className="w-4">ID</th>
+								<th className="w-48">Start Time</th>
+								<th className="w-48">End Time</th>
+								<th className="w-12">Elapsed Time</th>
+								<th className="w-1/3 break-words">Work Done</th>
+								<th className="w-12">Amount</th>
+							</tr>
+						</thead>
+						<tbody>
+							{/* body */}
+							{laps.map((lap, index) => {
+								return (
+									<tr key={index + 1} className="text-2xl">
+										<td className="w-fit text-2xl">{index + 1}</td>
+										<td className="text-2xl">{lap.getStartTime()}</td>
+										<td className="text-2xl">
+											{lap.getEndTime() === 0
+												? "Not finished yet"
+												: lap.getEndTime()}
+										</td>
+										<td className="text-2xl">
+											{lap.getCurrentHours()}h: {lap.getCurrentMinutes()}m:{" "}
+											{lap.getCurrentSeconds()}s
+										</td>
+										<td className="text-2xl break-words">
+											<textarea
+												className="textarea textarea-bordered w-full rounded-lg text-xl"
+												placeholder="Type here"
+												value={lap.getWorkDoneString()}
+												onChange={(e) => {
+													updateWorkDoneByID(lap.getId(), e.target.value);
+												}}
+											></textarea>
+										</td>
+										<td className="text-2xl">{lap.getAmount()}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				</div>
+			) : (
+				// tell user to start adding new laps
+				<div
+					className="flex justify-center items-center h-fit gap-16 m-10 outline p-6 rounded-xl outline-secondary min-h-64 text-2xl text-secondary flex-col"
+				>
+					<RandomSVG />
+					<div className="flex gap-1 items-center justify-center text-center">
+							<div>
+							No Laps Added Yet! Start by clicking
+							</div>
+							<StartButton className="w-12 h-12 text-accent transition-all duration-200 hover:scale-90" /> above.
+					</div>
+				</div>
+			)}
+
 
 			<div className="w-full flex flex-col justify-center items-center my-10 outline p-6">
 				<div className="font-bold  text-6xl">Stats</div>
